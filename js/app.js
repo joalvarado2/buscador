3973089fa859d992c756d5e2efe1c226ac3045f7
Jonathan Alvarado <jonathanalva92@gmail.com>
2020-10-27 17:32:09 -0500

    filtrando x marca y año►

--- js/app.js
+++ js/app.js
@@ -1,20 +1,76 @@
 // variables
-const resultado = document.querySelector("#resultado");
+const marca = document.querySelector("#marca");
 const year = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+
+// contenedor para los resultados
+const resultado = document.querySelector("#resultado");
+
 const max = new Date().getFullYear();
 const min = max - 10;
 
+//generando objeto
+const datosBusqueda= {
+    marca : "",
+    year : "",
+    minimo : "",
+    maximo : "",
+    puertas : "",
+    transmision : "",
+    color : "",
+}
 
 //eventos
 document.addEventListener("DOMContentLoaded", () => {
-    mostrarAutos();
+    mostrarAutos(autos);
 
     llenarSelect();
 })
 
+//listener para los select de busqueda
+marca.addEventListener("change", e => {
+    datosBusqueda.marca = e.target.value;
+
+    filtrarAuto();
+});
+
+year.addEventListener("change", e => {
+    datosBusqueda.year = parseInt(e.target.value);
+
+    filtrarAuto();
+});
+
+minimo.addEventListener("change", e => {
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener("change", e => {
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener("change", e => {
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener("change", e => {
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener("change", e => {
+    datosBusqueda.color = e.target.value;
+});
+
 
 //funciones
-function mostrarAutos() {
+function mostrarAutos(autos) {
+
+    limpiarHTML();
+
     autos.forEach((auto) => {
         const {marca, modelo, year, puertas, transmision, precio, color} = auto;
         const autoHTML = document.createElement("p");
@@ -29,6 +85,13 @@ function mostrarAutos() {
     });
 }
 
+//limpiar HTML
+function limpiarHTML() {
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
 function llenarSelect() {
 
     for( let i = max; i >= min; i--){
@@ -37,4 +100,28 @@ function llenarSelect() {
         opcion.textContent = i;
         year.appendChild(opcion);
     }
+}
+
+// funcion que filtra en base a la búsqueda
+function filtrarAuto() {
+    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear );
+    console.log(resultado)
+
+    mostrarAutos(resultado)
+}
+
+function filtrarMarca(auto) {
+    const { marca } = datosBusqueda;
+    if(marca){
+        return auto.marca === marca;
+    }
+    return auto;
+}
+
+function filtrarYear(auto) {
+    const { year } = datosBusqueda;
+    if(year){
+        return auto.year === year;
+    }
+    return auto;
 }
