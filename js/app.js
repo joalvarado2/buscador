a83b3d7a69c24f28bc693df9593359c4b3853034
Jonathan Alvarado <jonathanalva92@gmail.com>
2020-10-27 16:25:18 -0500

    creando html y estilos

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// variables
+const resultado = document.querySelector("#resultado");
+const year = document.querySelector("#year");
+const max = new Date().getFullYear();
+const min = max - 10;
+
+
+//eventos
+document.addEventListener("DOMContentLoaded", () => {
+    mostrarAutos();
+
+    llenarSelect();
+})
+
+
+//funciones
+function mostrarAutos() {
+    autos.forEach((auto) => {
+        const {marca, modelo, year, puertas, transmision, precio, color} = auto;
+        const autoHTML = document.createElement("p");
+
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} - Transmision: ${transmision} - Precio: ${precio} - 
+            Color: ${color}
+        `
+
+        // insertar en el html
+        resultado.appendChild(autoHTML);
+    });
+}
+
+function llenarSelect() {
+
+    for( let i = max; i >= min; i--){
+        const opcion = document.createElement("option");
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion);
+    }
+}
